ba89ccd99cbe81f720c623d4f606597366c78d4e
Dries Swinnen <36841875+d-ries@users.noreply.github.com>
2025-09-26 11:25:31 +0200

    Remove testDepositarValorComSaldoZero method
    Removed test for depositing value with zero balance.

--- src/test/java/ContaMockTest.java
+++ src/test/java/ContaMockTest.java
@@ -7,7 +7,6 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
 @PrepareForTest(ContaMock.class)
 public class ContaMockTest {
 
@@ -24,30 +23,7 @@ public class ContaMockTest {
         valorSac = 7.00;
     }
 
-    @Test
-    public void testDepositarValorComSaldoZero() throws Exception {
-
-        //arrange
-        ContaMock contaMock =  new ContaMock();
-        final String METHOD="getSaldo";
-
-        ContaMock spy = PowerMockito.spy(contaMock);
-        PowerMockito.when(spy, METHOD)
-                    .thenReturn( (Double) 1000.00);
-
-        double expect = 1010;
-        double actual = 0;
-
-        //act
-        actual = spy.depositarValor(valorDep);
-
-        //assert
-        Assert.assertEquals(expect, actual, 0);
-        PowerMockito.verifyPrivate(spy, Mockito
-                .times(1))
-                .invoke("getSaldo");
-
-    }
+  
 
     @Test
     public void testSacarValorComSaldoMaiorQueZero() throws Exception {
